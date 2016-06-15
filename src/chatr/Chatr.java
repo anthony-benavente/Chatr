902c0abe652b0b2f749e1f2c64d4b4d69cb6fc45
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-06-15 14:42:02 -0400

    Removed swing from the project

--- src/chatr/Chatr.java
+++ src/chatr/Chatr.java
@@ -1,6 +1,11 @@
 package chatr;
 
-import chatr.gui.swing.ChatrWindow;
+import chatr.client.ChatrClient;
+import chatr.client.ChatrClientException;
+import chatr.client.ChatrUdpClient;
+import chatr.gui.ChatrCommandWindow;
+
+import java.util.Scanner;
 
 /**
  * TODO: Describe me
@@ -9,8 +14,71 @@ import chatr.gui.swing.ChatrWindow;
  * @version 6/12/16
  */
 public class Chatr {
+
+    private ChatrCommandWindow window;
+
+    private Scanner in;
+
+    private String username;
+
+    private ChatrClient client;
+
+    public Chatr() {
+        window = new ChatrCommandWindow();
+        window.setVisible(true);
+        in = new Scanner(System.in);
+    }
+
+    public void start() throws ChatrClientException {
+        createClient();
+        client.go();
+
+        String input;
+        do {
+            System.out.print(username + ": ");
+            input = in.nextLine();
+            if (!input.equals("\\quit")) {
+                try {
+                    client.sendMessage(input);
+                } catch (ChatrClientException e) {
+                    e.printStackTrace();
+                }
+            }
+        } while (!input.equals("\\quit"));
+    }
+
+    private void createClient() {
+        boolean validHostAndPort;
+        do {
+            try {
+                String address = prompt("Enter the host address: ");
+                System.out.print("Enter the port: ");
+                int port = in.nextInt();
+                in.nextLine(); // Flush it out.
+                username = prompt("Enter your username: ");
+                client = new ChatrUdpClient(address, port, username);
+                validHostAndPort = true;
+            } catch (ChatrClientException e) {
+                validHostAndPort = false;
+            }
+        } while (!validHostAndPort);
+
+        client.addActionListener(e -> {
+            window.appendMessage(e.getActionCommand());
+        });
+    }
+
+    private String prompt(String prompt) {
+        System.out.print(prompt);
+        return in.nextLine();
+    }
+
     public static void main(String[] args) {
-        ChatrWindow window = new ChatrWindow();
-        window.start();
+        Chatr app = new Chatr();
+        try {
+            app.start();
+        } catch (ChatrClientException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/chatr/gui/ChatrCommandWindow.java
+++ src/chatr/gui/ChatrCommandWindow.java
@@ -1,4 +1,4 @@
-package chatr.gui.cmd;
+package chatr.gui;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
